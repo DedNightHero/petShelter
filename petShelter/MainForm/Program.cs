37cf459891ea9bdd706916d02cb4192cfcc55fa4
Dmitriy Feshchenko <feshchenko.dn@gmail.com>
2018-11-14 18:08:35 +0700

    Конец)

--- petShelter/MainForm/Program.cs
+++ petShelter/MainForm/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace MainForm
@@ -11,12 +9,26 @@ namespace MainForm
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Authorization());
+            if (System.Diagnostics.Process.GetProcessesByName(Application.ProductName).Length > 1)
+            {
+                IntPtr hWnd = System.Diagnostics.Process.GetProcessesByName(Application.ProductName)[0].MainWindowHandle;
+                SetForegroundWindow(hWnd);
+                ShowWindow(hWnd, 5);
+                return;
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Authorization());
+            }
         }
     }
 }
--- petShelter/MainForm/Properties/AssemblyInfo.cs
+++ petShelter/MainForm/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("MainForm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PetShelter")]
+[assembly: AssemblyDescription("Программа для питомника")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MainForm")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Altstu")]
+[assembly: AssemblyProduct("PetShelter")]
+[assembly: AssemblyCopyright("Copyright FeshchenkoDN ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.5")]
+[assembly: AssemblyFileVersion("2.0.0.5")]
